49c6a366caf9ccb57dbe20a4c8d8a5f6c9461756
DESKTOP-FPH99T9\Basti <acedia247@gmail.com>
2018-03-12 17:44:15 +0100

    .

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,17 +30,7 @@ const TemplateWrapper = ({ children }) => (
       {children()}
     </div>
     <Footer />
-    <script>
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-              document.location.href = "/admin/";
-            });
-          }
-        })
-      }
-</script>
+
   </div>
 )
 

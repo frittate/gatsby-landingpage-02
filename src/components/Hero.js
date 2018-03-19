80d987ec9b8fc2123ce803cd8bcd86cf3bd14a47
DESKTOP-FPH99T9\Basti <acedia247@gmail.com>
2018-03-19 12:15:14 +0100

    new config

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,14 @@
 import React from 'react'
-
-const style = {
-  h1: {
-    textAlign: "center",
-    color: "red",
-  }
-}
+import style from '../style/styles'
 
 const Hero = props => {
   console.log(props);
   return (
-    <h1 style={style.h1}>{props.title}</h1>
+    <div class="hero-text">
+        <h1 style={style.h1}>{props.title}</h1>
+        <p><span style={style.subhead}>The Future of Digital Learning</span></p>
+        <a href="#mc_embed_signup" class="btn">Join As A Tester Now</a> 
+</div>     
   )
 }
 
---
+++ src/style/styles.js
@@ -0,0 +1,16 @@
+const style = {
+  h1: {
+    textAlign: "center",
+    color: "red",
+  },
+  subhead: {
+    color: "#ffffff",
+    fontSize: "4.5rem",
+    fontWeight: "700",
+    letterSpacing: "0.5rem",
+    marginTop: "0",
+    textTransform: "uppercase",
+  }
+}
+
+export default style;
